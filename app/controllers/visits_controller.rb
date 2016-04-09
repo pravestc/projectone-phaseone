1d25bf0afbd0c38e711cf46688006fa5bdc0bf11
pravestc <pravest@gmail.com>
2016-04-09 10:36:55 -0500

    Revised version - Complete

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,15 @@
 class VisitsController < ApplicationController
   def index
-    @visit = Site.find(params[:site_id]).visit
+    if Site.exists?(params[:site_id])
+	@visit = Site.find(params[:site_id]).visit
         @visit.count = @visit.count + 1
-    @visit.save
+        @visit.save
+    else
+        @site = Site.new
+        @site.id = params[:site_id]
+	@site.title = @site.id
+        @site.save
+        @site.visit = Visit.new(count: 1)
+    end
   end
 end
